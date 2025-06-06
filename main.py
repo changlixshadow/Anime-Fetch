4b159185cadb3868c06f4c7249d33659ad716158
changlixshadow <amanthb82@gmail.com>
2025-06-06 19:34:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -290,9 +290,5 @@ def run_bot():
     app_.run_polling()
 
 if __name__ == "__main__":
-    # Start Telegram bot in a separate thread
-    threading.Thread(target=run_bot, daemon=True).start()
+    run_bot()
 
-    # Run Flask app (main thread)
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
